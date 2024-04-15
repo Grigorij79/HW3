95e4b87801aedce910edc913549e94a5528582ca
KovGr <KovGrDm@outlook.com>
2024-04-16 00:14:37 +0300

    HW3

--- src/main/java/org/example/SumCalculator.java
+++ src/main/java/org/example/SumCalculator.java
@@ -1,8 +1,21 @@
 package org.example;
 
-public class Factorial {
-    public long calc (int n){
-        return 2;
+public class SumCalculator {
+    public int sum(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("Input should be > 0");
+        }
+
+        int rezult = 0;
+        for (int i = 1; i <= n; i++) {
+            rezult += i;
+
+            if (rezult < 0 ) {
+                throw new IllegalArgumentException("Input " + n + " to big");
+            }
+        }
+
+        return rezult;
     }
 
 }
--- src/test/java/SumCalculatorTest.java
+++ src/test/java/SumCalculatorTest.java
@@ -1,17 +1,59 @@
-import org.example.Factorial;
+import org.example.SumCalculator;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
+public class SumCalculatorTest {
+    private SumCalculator sumCalc;
 
-public class FactoriallTest {
-    @Test
-    public void testT(){
-        long ackyual = new Factorial().calc(0);
-        Assertions.assertEquals(1,ackyual);
+    @BeforeEach
+    public void beforeEach() {
+        sumCalc = new SumCalculator();
     }
+
     @Test
-    public void testTT(){
-        Assertions.assertEquals(2,new Factorial().calc(2));
+    public void testThat1Correct() {
+        int actual = sumCalc.sum(1);
+
+        int expected = 1;
+
+        Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    public void testThat3Correct() {
+        int actual = sumCalc.sum(3);
+
+        int expected = 6;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testThat0Correct() {
+        Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> sumCalc.sum(0)
+        );
+    }
+
+    @Test
+    public void testThatNegativeInputCorrect() {
+        Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> sumCalc.sum(-1)
+        );
+    }
+
+    @Test
+    public void testThatBigInputCorrect() {
+        int maxN = 65536;
+
+        Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> sumCalc.sum(maxN + 1)
+        );
+    }
+
 }
